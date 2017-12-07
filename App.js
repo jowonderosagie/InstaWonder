584650aac647638889f379db0ff917d104a746d1
jowonderosagie <jowonderosagie@gmail.com>
2017-12-07 14:40:17 +0100

    Made some adjustments to our View

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, ImageBackground, Image, ScrollView } from 'react-native';
+import Dimensions from 'Dimensions';
 
-export default class App extends React.Component {
+
+const windowSize = Dimensions.get('window');
+
+
+export default class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+  loginScreenComponent = () => {
+    return (
+      <ImageBackground
+        style={viewStyles.container}
+        resizeMode={'cover'}
+        source={require('./src/images/instabackground4.jpg')}
+        >
+
+        <ScrollView style={viewStyles.scrollViewStyles}>
+
+          <Image
+            style={viewStyles.instagramLogo}
+            resizeMode={'contain'}
+            source={require('./src/images/instagram-text-logo.png')}
+          />
+
+        </ScrollView>
+
+
+      </ImageBackground>
+    );
+  }
+
+
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      this.loginScreenComponent()
     );
+
   }
 }
 
-const styles = StyleSheet.create({
+const viewStyles = {
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  instagramLogo: {
+    width: (0.45 * windowSize.width),
+    height: (0.15 * windowSize.height),
+    marginBottom: 25,
+    alignSelf: 'center'
+  },
+  scrollViewStyle: {
+    paddingTop: '38%'
+  }
+
+};
